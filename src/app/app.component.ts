487b7f85ab5e35c16400e436f1193d255b43fb2e
Jorge Guerrero <jorgegro1999@gmail.com>
2018-07-05 12:09:49 -0500

    first blood

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = "Jorge";
+  active = true;
+  totalTeams = 4;
+  teams = [
+    {
+      teamName: 'Chupacabras',
+      players: ['Michael', 'Daira', 'Tiny','Kenny', 'Johnny'],
+      teamHome: 'Mission'
+    },
+    {
+      teamName: 'Wrench Dodgers',
+      players: ['Cesare', 'Erick', 'Victor', 'Jess', 'Jeremy'],
+      teamHome: 'New York'
+    },
+    {
+      teamName: 'Los Tamles',
+      players: ['Jorge', 'Jacqueline', 'Steven', 'Hiram', 'Luis'],
+      teamHome: 'Los Angeles'
+    },
+    {
+      teamName: 'Weeaboos',
+      players: ['Ernesto', 'Joshua', 'Emmanuel', 'Tony', 'Roxane'],
+      teamHome: 'Tokyo'
+    },
+  ];
 }
